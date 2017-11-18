f070eced2e6d0a88a698982a147e6bfb0567160a
Irina Kolbun <kolbunirina01@gmail.com>
2017-11-18 23:00:15 +0000

    Show page for 1-2 items

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,51 @@ class TodoController < ApplicationController
   def index
   end
   def show
-  @todo_description = "hh"
-  @todo_pomodoro_estimate = 89
+  todo_id = params[:id]
+
+  if todo_id == '1'
+    @todo_description = "Make the curriculum"
+    @todo_pomodoro_estimate = 4
+
+  elsif todo_id == '2'
+    @todo_description = "Buy workshop supplies"
+    @todo_pomodoro_estimate = 3
+  end
 end
 end
+
+
+ # If the user types in /todo/show/1 to the URL
+  # make @todo_description equal "Make the curriculum"
+  # and
+  # make @todo_pomodoro_estimate equal "4"
+  #
+  # If the user types in /todo/show/2 to the URL
+  # make @todo_description equal "Buy workshop supplies"
+  # and
+  # make @todo_pomodoro_estimate equal "3"
+  #
+  # If the user types in /todo/show/3 to the URL
+  # make @todo_description equal "Meet with volunteer trainers"
+  # and
+  # make @todo_pomodoro_estimate equal "2"
+  #
+  # If the user types in /todo/show/4 to the URL
+  # make @todo_description equal "Order food for Saturday and Sunday"
+  # and
+  # make @todo_pomodoro_estimate equal "1"
+  #
+  # If the user types in /todo/show/5 to the URL
+  # make @todo_description equal "Check pre-work assignments"
+  # and
+  # make @todo_pomodoro_estimate equal "2"
+  #
+  # If the user types in /todo/show/6 to the URL
+  # make @todo_description equal "Send workshop location to all the students"
+  # and
+  # make @todo_pomodoro_estimate equal "1"
+  #
+  # If the user types in /todo/show/7 to the URL
+  # make @todo_description equal "Have a great workshop"
+  # and
+  # make @todo_pomodoro_estimate equal "0"
