16f0fc9e1b856d1177198e53e9d4f6da394a06bb
Irina Kolbun <kolbunirina01@gmail.com>
2017-11-19 23:19:10 +0000

    Final2

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -17,7 +17,7 @@ end
   def update
   t = Todo.find_by_id(params['id'])
   t.description = params['description']
-  t.pomodoro_estimate = params['pomodoro-estimate']
+  t.pomodoro_estimate = params['pomodoro_estimate']
   t.save
   redirect_to "/todo/show/#{t.id}"
 end
